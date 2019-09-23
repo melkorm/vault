0dc7f7c888a331dce851957e0020c47fe548a268
Becca Petrin <beccapetrin@gmail.com>
2019-09-23 10:47:03 -0700

    deadlock

--- sdk/helper/locksutil/locks_test.go
+++ sdk/helper/locksutil/locks_test.go
@@ -1,10 +1,25 @@
 package locksutil
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func Test_CreateLocks(t *testing.T) {
 	locks := CreateLocks()
 	if len(locks) != 256 {
 		t.Fatalf("bad: len(locks): expected:256 actual:%d", len(locks))
+	}
+}
+
+func TestManyLocks(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	locks := CreateLocks()
+	for i := 0; i < 100; i++ {
+		lockName := fmt.Sprintf("%d", r.Int())
+		lock := LockForKey(locks, lockName)
+		lock.Lock()
 	}
 }
